55b2d399db0e79b6b118400ad3b0bc2eb146d0cf
islam-alsaeed <islam.alsaeed@gmail.com>
2021-06-09 23:37:25 +0200

    t1

---
+++ components/footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <div class="columns container">
+      <div class="column">
+        <p>hey there ^_^</p>
+      </div>
+    <div class="column has-text-right">
+      <p>I hate web ^_^</p>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VmFooter',
+}
+</script>
+
+<style lang="scss" scoped>
+ .footer {
+   background: #b9e2fc;
+   color: black;
+ }
+</style>
---
+++ components/hero/Hero.vue
@@ -0,0 +1,30 @@
+// the blue box
+<template>
+  <div class="hero is-medium is-bold">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          petsyyyyyyyyyyyy
+          helloo
+        </h1>
+        <br>
+        <h2 class="subtitle">
+          god bless us
+        </h2>
+        <div>
+          <p>branch 7</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  background: rgb(81, 186, 252);
+}
+h1,
+h2 {
+  color: #fff;
+}
+</style>
